0df18de4e2db13623e2caf238727b2e4aec47b72
ahmedhakeem22 <58742511+ahmedhakeem22@users.noreply.github.com>
2026-02-14 05:13:55 +0300

    chore: Remove all Laravel project configuration and dependency files.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,41 +2,24 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        Model::unguard();
-        Schema::disableForeignKeyConstraints();
+        // User::factory(10)->create();
 
-        $this->call([
-            RoleSeeder::class,
-            // PermissionSeeder::class, // قم بإلغاء التعليق إذا أنشأته
-            FloorSeeder::class,
-            AssessmentItemSeeder::class,
-            EmployeeAndUserSeeder::class,
-
-          
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
-
-        // 2. بيانات تعتمد على ما سبق
-        $this->call([
-            RoomAndBedSeeder::class, // يعتمد على Floors
-        ]);
-        
-        // 4. بيانات الجدولة
-        // $this->call(EmployeeShiftSeeder::class); // قم بإلغاء التعليق إذا أنشأته
-
-        Schema::enableForeignKeyConstraints();
-        Model::reguard();
-        
     }
 }
